fe6521ee0bf953f4c56d1413a66a950a018d86e9
henderson907 <henderson.freddy907@gmail.com>
2025-04-30 12:35:05 +0100

    managed to get basic scraping api and data retrieval done for aldi

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+# Allows us to use a webscraping API to retrieve the html and subsequent product card
+payload = { 'api_key': '68f66e079cf6b42e57365a68fd239b6f', 'url': 'https://www.aldi.co.uk/results?q=orange' }
+r = requests.get('https://api.scraperapi.com/', params=payload)
+
+soup = BeautifulSoup(r.text, 'html.parser')
+print(soup.find(class_="product-tile"))
+
+aldi_item = soup.find(class_="product-tile__name")
+
+aldi_quant = str(soup.find(class_="product-tile__unit-of-measurement"))
+aldi_quant = aldi_quant[93:]
+aldi_quantity = int(re.search("[0-9]+", aldi_quant).group(0))
+
+# Price is price per item!
+aldi_pr = str(soup.find(class_="base-price__regular"))
+aldi_pr = aldi_pr[40:]
+aldi_price = float(re.search("[0-9]+\.[0-9]+",aldi_pr).group(0)) / aldi_quantity
+
+print(aldi_item)
+print(aldi_quantity)
+print(aldi_price)
