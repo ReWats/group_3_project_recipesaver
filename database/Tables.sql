25ad089e6423731c4acc039d1c723fdd1070d09e
henderson907 <henderson.fredd907@gmail.com>
2025-04-30 23:50:51 +0100

    removed unnecessary tables

--- database/Tables.sql
+++ database/Tables.sql
@@ -19,28 +19,8 @@ CREATE TABLE IF NOT EXISTS recipe_items (
     FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id) ON DELETE CASCADE
 );
 
--- Supermarket_prices Table (empty table to store live scraped prices)
-CREATE TABLE IF NOT EXISTS supermarket_prices (
-    price_id INT AUTO_INCREMENT PRIMARY KEY,
-    scraped_item_name VARCHAR(255) NOT NULL, -- e.g., "Large Free-Range Eggs"
-    quantity VARCHAR(50),
-    price DECIMAL(10,2) NOT NULL,
-    supermarket VARCHAR(50) NOT NULL,
-    last_updated DATETIME NOT NULL -- When the price was scraped
-);
--- Temp ingredients (one-time use, users do not save items as recipes)
-CREATE TABLE IF NOT EXISTS temp_ingredients (
-	temp_id INT AUTO_INCREMENT PRIMARY KEY,
-    session_id VARCHAR(255),
-    item_name VARCHAR(255) NOT NULL,
-    quantity DECIMAL (10,2) DEFAULT 1,
-    unit VARCHAR(50) DEFAULT 'each'
-);
-
 -- Indexes
 CREATE INDEX idx_recipe_items_recipe_id ON recipe_items(recipe_id);
-CREATE INDEX idx_prices_item ON supermarket_prices(scraped_item_name);
-CREATE INDEX idx_temp_session ON temp_ingredients(session_id);
 
 -- Sample data: Pancake Recipe
 INSERT INTO recipes (recipe_name)
@@ -56,44 +36,3 @@ VALUES
 (@pancake_id, 'Sunflower oil', 1, 'tbsp'),
 (@pancake_id, 'Lemon', 1, 'each'),
 (@pancake_id, 'Caster sugar', 0.02, 'kg');
-
--- Stored procedure: cheapest single-store total
-DELIMITER $$
-CREATE PROCEDURE get_cheapest_store(IN sess_id VARCHAR(255))
-BEGIN
-    SELECT
-       sp.supermarket,
-       SUM(sp.price) AS total_cost
-
-      FROM temp_ingredients ti
-      JOIN supermarket_prices sp
-        ON sp.scraped_item_name = ti.item_name
-       WHERE ti.session_id = sess_id
-        AND sp.last_updated > NOW() - INTERVAL 1 HOUR
-       GROUP BY sp.supermarket
-       ORDER BY total_cost
-       LIMIT 1;
-      END $$
-      DELIMITER ;
-
--- Stored procedure: Cheapest per-item breakdown
-DELIMITER $$
-CREATE PROCEDURE get_cheapest_per_item(IN sess_id VARCHAR(255))
-BEGIN
-  SELECT
-    ti.item_name,
-    MIN(sp.price)                     AS cheapest_price,
-    SUBSTRING_INDEX(
-      GROUP_CONCAT(
-        sp.supermarket
-        ORDER BY sp.price ASC
-        SEPARATOR ','
-      ), ',', 1
-    )                                  AS cheapest_supermarket
-  FROM temp_ingredients ti
-  JOIN supermarket_prices sp
-    ON sp.scraped_item_name = ti.item_name
-  WHERE ti.session_id = sess_id
-  GROUP BY ti.item_name;
-END $$
-DELIMITER ;
